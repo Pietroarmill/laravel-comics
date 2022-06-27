150ad6bdef96cfb433c8a40a6d8aba360a5131ff
Pietroarmill <pietroarmillotta38@gmail.com>
2022-06-27 23:33:05 +0200

    completo

--- resources/views/single-serie.blade.php
+++ resources/views/single-serie.blade.php
@@ -6,7 +6,11 @@
     <div class="row-blue">
         <div class="container">
             <div class="serie-card">
+              <div class="serie-card-box">
                 <img src="{{ $serie['thumb'] }}" alt="">
+                <span class="tag tag-top">COMIC BOOK</span>
+                <span class="tag tag-bottom">VIEW GALLERY</span>
+              </div>
             </div>
         </div>
     </div>
@@ -16,16 +20,22 @@
             <div class="col-left">
                 <h3>{{ $serie['title'] }}</h3>
                 <div class="price">
-                    
+                    <div class="price-col price-left">
+                      <div class="item-price">U.S. Price: <span>{{ $serie['price'] }}</span></div>
+
+                      <div class="available">AVALAIBLE</div>
+                    </div>
+
+                    <div class="price-col price-right">
+                      <span>Check Availability</span> <i class="fas fa-sort-down"></i>
+                    </div>
                 </div>
                 <p>{{ $serie['description'] }}</p>
             </div>
 
             <div class="col-right">
-                <span>AD</span>
-                <div class="banner">
-                    pubblicità
-                </div>
+                <p>ADVERTISEMENT</p>
+                <img src="{{ asset('images/adv.jpg') }}" alt="">
             </div>
         </section>
     </div>
@@ -40,7 +50,7 @@
                     <h5>Art by:</h5>
                     <p>
                       @foreach ($serie['artists'] as $item)
-                        <span>{{ $item }},</span>
+                        <span>{{ $item }}<span class="black">,</span></span>
                       @endforeach 
                     </p>
                 </div>
@@ -48,18 +58,18 @@
                     <h5>Written by:</h5>
                     <p>
                       @foreach ($serie['writers'] as $item)
-                        <span>{{ $item }},</span>
+                        <span>{{ $item }}<span>,</span></span>
                       @endforeach 
                     </p>
                 </div>
             </div>
 
             <div class="col-right">
-                <h4>Spec</h4>
+                <h4>Specs</h4>
                 <div class="specifications-item">
                     <h5>Series:</h5>
                     <p>
-                      <span>{{ $serie['series'] }}</span>
+                      <span class="upper-c">{{ $serie['series'] }}</span>
                     </p>
                 </div>
                 <div class="specifications-item">
@@ -79,42 +89,30 @@
           <div class="shop-tag">
 
             <div class="shop-tag-item">
-              <h5>Digital</h5>
+              <h5>digital comics</h5>
               <div class="img">
-                img
-                <img src="" alt="">
+                <img src="{{ asset('images/buy-comics-digital-comics.png') }}" alt="">
               </div>
             </div>
 
             <div class="shop-tag-item">
-              <h5>Digital</h5>
+              <h5>shop dc</h5>
               <div class="img">
-                img
-                <img src="" alt="">
+                <img src="{{ asset('images/buy-comics-subscriptions.png') }}" alt="">
               </div>
             </div>
 
             <div class="shop-tag-item">
-              <h5>Digital</h5>
+              <h5>comic shop locator</h5>
               <div class="img">
-                img
-                <img src="" alt="">
+                <img src="{{ asset('images/buy-comics-shop-locator.png') }}" alt="">
               </div>
             </div>
 
             <div class="shop-tag-item">
-              <h5>Digital</h5>
+              <h5>subscriptions</h5>
               <div class="img">
-                img
-                <img src="" alt="">
-              </div>
-            </div>
-
-            <div class="shop-tag-item">
-              <h5>Digital</h5>
-              <div class="img">
-                img
-                <img src="" alt="">
+                <img src="{{ asset('images/buy-comics-merchandise.png') }}" alt="">
               </div>
             </div>
 
