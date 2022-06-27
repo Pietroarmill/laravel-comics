87a716b0954ae2d3597a75d83c6b2d0d9d420efe
Pietroarmill <pietroarmillotta38@gmail.com>
2022-06-27 16:55:46 +0200

    add footer-bottom

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,64 +1,111 @@
 <footer>
-  <section class="footer-top">
-    <div class="bg-footer-top">
-      <div class="container">
-        <div class="row">
+    <section class="footer-top">
+        <div class="bg-footer-top">
+            <div class="container">
 
-          <div class="col col-links">
-            <h3>dc comics</h3>
-            <ul>
-              @foreach ($dc_comics_links as $link)
-              <li>
-                <a href="">{{ $link }}</a>
-              </li>
-              @endforeach
-            </ul>
-            <h3>shop</h3>
-            <ul>
-              @foreach ($shop_links as $link)
-                  <li>
-                    <a href="">{{ $link }}</a>
-                  </li>
-              @endforeach
-            </ul>
-          </div>
-  
-          <div class="col col-links">
-            <h3>dc</h3>
-            <ul>
-              @foreach ($dc_links as $link)
-                  <li>
-                    <a href="">{{ $link }}</a>
-                  </li>
-              @endforeach
-            </ul>
-          </div>
-  
-          <div class="col col-links">
-            <h3>sites</h3>
-            <ul>
-              @foreach ($sites_links as $link)
-                  <li>
-                    <a href="">{{ $link }}</a>
-                  </li>
-              @endforeach
-            </ul>
-          </div>
+                <section class="links-section">
 
-          <div class="col-links"></div>
-  
-          <div class="col col-logo"></div>
+                    <div class="col-left">
+
+                        <div class="col col-links">
+                            <h3>dc comics</h3>
+                            <ul>
+                                @foreach ($dc_comics_links as $link)
+                                    <li>
+                                        <a href="">{{ $link }}</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                            <h3>shop</h3>
+                            <ul>
+                                @foreach ($shop_links as $link)
+                                    <li>
+                                        <a href="">{{ $link }}</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+    
+                        <div class="col col-links">
+                            <h3>dc</h3>
+                            <ul>
+                                @foreach ($dc_links as $link)
+                                    <li>
+                                        <a href="">{{ $link }}</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+    
+                        <div class="col col-links">
+                            <h3>sites</h3>
+                            <ul>
+                                @foreach ($sites_links as $link)
+                                    <li>
+                                        <a href="">{{ $link }}</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-right"></div>
+
+                </section>
+
+            </div>
 
         </div>
+    </section>
 
-        <p>All site Content TM and copirigth 2020 DC Entertainment, unless otherwise <span>noted here</span>. All rights reserved.</p>
-        <p><span>Coockies Setting</span></p>
-      </div>
+    <section class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-left">
+                    <div class="btn-sign">sign-up now!</div>
+                </div>
 
-    </div>
-  </section>
-
-  <section class="footer-bottom">
-
-  </section>
+                <div class="col-right">
+                    <span>Follow us</span>
+                    <ul>
+                        <li>
+                            <div class="social-logo">
+                                <a href="">
+                                    <img src="{{ asset("images/footer-facebook.png") }}" alt="">
+                                </a>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="social-logo">
+                                <a href="">
+                                    <img src="{{ asset("images/footer-twitter.png") }}" alt="">
+                                </a>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="social-logo">
+                                <a href="">
+                                    <img src="{{ asset("images/footer-youtube.png") }}" alt="">
+                                </a>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="social-logo">
+                                <a href="">
+                                    <img src="{{ asset("images/footer-pinterest.png") }}" alt="">
+                                </a>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="social-logo">
+                                <a href="">
+                                    <img src="{{ asset("images/footer-periscope.png") }}" alt="">
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 </footer>
